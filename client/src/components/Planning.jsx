e0dac35e30369abc29bbf8b9f6469733d362b780
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-03-07 11:24:31 -0800

    updates: layout changes, 3-7-24

--- client/src/components/Planning.jsx
+++ client/src/components/Planning.jsx
@@ -14,6 +14,7 @@ const Planning = () => {
           <section className="content">
              {/* Problem Solving Process */}
              <h2>Code Problem Solving Process</h2>
+             <div className='card'>
             <h3>Understand the Problem:</h3> 
 <p>
 Read carefully and identify the key components.</p>
@@ -23,12 +24,13 @@ Read carefully and identify the key components.</p>
 <h3>Execute:</h3>
 <p> Code with focus, using VS Code or Sublime Text for a better coding experience. Both editors support extensions and plugins that can facilitate your coding process.</p>
 <h3>Review:</h3>
-<p>Test your code thoroughly. Use Git for version control to manage changes and collaborate with others. GitHub offers actions and CI/CD workflows to automate your testing and deployment processes.</p>
+<p>Test your code thoroughly. Use Git for version control to manage changes and collaborate with others. GitHub offers actions and CI/CD workflows to automate your testing and deployment processes.</p></div>
+             
 
             {/* Project Proposal Preparation */}
-            <h2>Project Planning and Proposal:</h2>
 
-<h3>Project Proposal:</h3>  <p>Start with a clear and concise paragraph. Tools like Google Docs can be useful for drafting and sharing documents.</p>
+            <h2>Project Planning and Proposal:</h2>
+            <div className='card'><h3>Project Proposal:</h3>  <p>Start with a clear and concise paragraph. Tools like Google Docs can be useful for drafting and sharing documents.</p>
 
 <h3>MVP Definition:</h3><p> Define the scope of your MVP to ensure you have a focused and attainable goal. Trello or Asana can be used for project management and to track progress. </p>
 
@@ -37,11 +39,14 @@ Read carefully and identify the key components.</p>
 <h3>Mind Mapping:</h3><p> Plan your application's architecture and components. MindMeister or Coggle are great tools for creating mind maps and visualizing your project structure. </p>
 
 
-<p>By following these guidelines and utilizing the suggested tools and resources, you can enhance your development workflow, from problem-solving to project planning and execution.</p>
+<p>By following these guidelines and utilizing the suggested tools and resources, you can enhance your development workflow, from problem-solving to project planning and execution.</p></div>
+
+
 
             {/* Agile and Scrum */}
             <h2>Agile and Scrum</h2>
-            <p>Understanding Agile and Scrum is crucial for modern software development. These methodologies promote adaptive planning, evolutionary development, and flexibility. Learn more about Agile from the <a href="https://agilemanifesto.org/">Agile Manifesto</a> and about Scrum from <a href="https://www.scrum.org/resources/what-is-scrum">Scrum.org</a>.</p>
+            <div className='card'><p>Understanding Agile and Scrum is crucial for modern software development. These methodologies promote adaptive planning, evolutionary development, and flexibility. Learn more about Agile from the <a href="https://agilemanifesto.org/">Agile Manifesto</a> and about Scrum from <a href="https://www.scrum.org/resources/what-is-scrum">Scrum.org</a>.</p></div>
+            
 
           </section>
         </section>
--- client/src/components/Resources.jsx
+++ client/src/components/Resources.jsx
@@ -9,46 +9,48 @@ const Resources = () => {
           <h1 className="page-title">Helpful Resources</h1>
           <section className="content">
             {/* Existing resources */}
-            <div className='card'><h2>HTML</h2>
+            <h2>HTML</h2>
+            <div className='card'>
             <p>HTML (HyperText Markup Language) is the standard markup language for creating web pages. You can learn HTML from the 
             </p><a href="https://developer.mozilla.org/en-US/docs/Web/HTML">Mozilla Developer Network (MDN) documentation</a>.</div>
-            
-              <div className='card'><h2>CSS</h2>
+            <h2>CSS</h2>
+              <div className='card'>
             <p>CSS (Cascading Style Sheets) is a style sheet language used for describing the presentation of a document written in HTML. You can learn CSS from the </p><a href="https://developer.mozilla.org/en-US/docs/Web/CSS">MDN documentation</a>.</div>
-            
-            <div className='card'><h2>JavaScript</h2>
+            <h2>JavaScript</h2>
+            <div className='card'>
             <p>JavaScript is a programming language that enables you to create dynamically updating content, control multimedia, animate images, and much more. You can learn JavaScript from the </p><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">MDN documentation</a>.</div>
-            
-            <div className='card'><h2>npm</h2>
+            <h2>npm</h2>
+            <div className='card'>
             <p>npm (Node Package Manager) is the default package manager for the JavaScript runtime environment Node.js. It's an indispensable tool for modern web development. Learn more about npm from the</p> <a href="https://docs.npmjs.com/">official npm documentation</a>.</div>
-            
-            <div className='card'><h2>Morgan</h2>
+            <h2>Morgan</h2>
+            <div className='card'>
             <p>Morgan is a popular HTTP request logger middleware for Node.js, perfect for monitoring requests during development. Find documentation and usage information on</p> <a href="https://github.com/expressjs/morgan">GitHub</a>.</div>
-            
-            <div className='card'><h2>dotenv</h2>
+            <h2>dotenv</h2>
+            <div className='card'>
             <p>dotenv is a module that loads environment variables from a .env file into process.env, simplifying the management of configuration settings. Documentation and usage information are available on </p><a href="https://github.com/motdotla/dotenv">GitHub</a>.</div>
-            
-            <div className='card'><h2>UUID</h2>
+            <h2>UUID</h2>
+            <div className='card'>
             <p>UUID (Universally Unique Identifier) provides a way to generate unique identifiers, essential for ensuring data uniqueness across components. Documentation is available on</p> <a href="https://www.npmjs.com/package/uuid">npm</a>.</div>
-            
-            <div className='card'><h2>Git</h2>
+            <h2>Git</h2>
+            <div className='card'>
             <p>Git is the backbone of version control for developers, allowing you to track and manage code changes. Learn Git from the</p> <a href="https://git-scm.com/documentation">official Git documentation</a>.</div>
-            
-            <div className='card'><h2>GitHub</h2>
+            <h2>GitHub</h2>
+            <div className='card'>
             <p>GitHub extends Git's capabilities with online hosting, making it easier to collaborate on projects. Learn about GitHub from the</p> <a href="https://docs.github.com/en">official GitHub documentation</a>.</div>
-            
-            <div className='card'><h2>Netlify</h2>
+            <h2>Netlify</h2>
+            <div className='card'>
             <p>Netlify offers hosting and serverless backend services for static websites and web applications with continuous deployment from Git across all of Netlify’s CDN. Find more on the </p><a href="https://docs.netlify.com/">Netlify documentation</a>.</div>
-            
-            <div className='card'><h2>Render</h2>
+            <h2>Render</h2>
+            <div className='card'>
             <p>Render is a comprehensive cloud platform offering a range of services from static site hosting to PostgreSQL databases, emphasizing ease of use and automation. Documentation is available on </p><a href="https://render.com/docs">Render documentation</a>.</div>
             
 
             {/* New IDE resources */}
-            <div className='card'><h2>Visual Studio Code (VS Code)</h2>
+            <h2>Visual Studio Code (VS Code)</h2>
+            <div className='card'>
             <p>Visual Studio Code is a lightweight but powerful source code editor which runs on your desktop and is available for Windows, macOS and Linux. It comes with built-in support for JavaScript, TypeScript and Node.js and has a rich ecosystem of extensions for other languages and runtimes. Learn more and download from the</p> <a href="https://code.visualstudio.com/">official website</a>.</div>
-            
-            <div className='card'><h2>Sublime Text</h2>
+            <h2>Sublime Text</h2>
+            <div className='card'>
             <p>Sublime Text is a sophisticated text editor for code, markup, and prose. It offers a slick user interface along with a host of features such as Goto Anything, multiple selections, command palette, and more, making coding faster and more efficient. Learn more and download from the </p><a href="https://www.sublimetext.com/">official website</a>.</div>
             
 
